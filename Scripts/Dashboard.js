8cbccfc7f11493d4188125202ff9f2bbf6943b2b
raleighsavage <raleighsavage04@gmail.com>
2024-09-17 15:42:18 -0500

    added weather api

--- Scripts/Dashboard.js
+++ Scripts/Dashboard.js
@@ -4,7 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Example weather API
     setTimeout(() => {
-        tempElement.textContent = '84°F';
-        conditionElement.textContent = 'Sunny';
+        // tempElement.textContent = '84°F';
+        // conditionElement.textContent = 'Sunny';
+        getWeatherForNorman();
     }, 1000);
+
+    async function getWeatherForNorman() {
+        try {
+          // Getting grid points for Norman, OK
+          const pointResponse = await fetch('https://api.weather.gov/points/35.2226,-97.4395');
+          const pointData = await pointResponse.json();
+          
+          //Using the gridId, gridX, gridY to get the forecast URL
+          const forecastUrl = pointData.properties.forecast;
+      
+          //Getting the forecast data from the URL
+          const forecastResponse = await fetch(forecastUrl);
+          const forecastData = await forecastResponse.json();
+          
+          // Getting the current forecast period
+          const currentPeriod = forecastData.properties.periods[0];
+          gi
+          // Changing visual elements to current conditions
+          tempElement.textContent = `${currentPeriod.temperature}`;
+          conditionElement.textContent = `${currentPeriod.shortForecast}`;
+      
+        } catch (error) {
+          console.error('Error fetching weather data:', error);
+        }
+      }
 });
