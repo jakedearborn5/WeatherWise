3020dbf5c9bdc6b63d910fb0441d18cbd41b3e83
Jacob Bowlware <jacobbowlware@gmail.com>
2024-12-05 22:09:51 -0600

    Clear search input after use

--- Scripts/WeatherLogic.js
+++ Scripts/WeatherLogic.js
@@ -12,7 +12,7 @@ const weatherStore = {
         footwear: [],
         accessories: []
     },
-    
+
     // This will include everything the API has to offer for the specific location
     weatherInfo: {
         temperature: -1,
@@ -75,26 +75,27 @@ export async function getHourWeather(latitude, longitude) {
         //console.log(weatherStore.weatherInfo); // Temp for testing purposes
         generateRecommendations();
 
+        // Clear search input
+        document.getElementById('location-input').value = '';
+
     } catch (error) {
         console.error("Error fetching weather data:", error);
     }
 }
 
 // Splitting the date from the API into something that's actually useful and readable
-function splitDate(currentDate)
-{
+function splitDate(currentDate) {
     const [divorcedDate, timeWithOffset] = currentDate.split("T");
 
     // Fixing time
-    const time = timeWithOffset.split(":").slice(0,1).join(":");
+    const time = timeWithOffset.split(":").slice(0, 1).join(":");
 
     return [divorcedDate, time];
 }
 
 // PLEASE CHANGE THIS FUNCTION IF YOU NEED TO FOR THE RECOMMENDATIONS
 // This was just my inital thought on how it should work but do whatever is needed
-function generateRecommendations()
-{
+function generateRecommendations() {
     // Clearing previous recommendations so we don't get duplicates
     weatherStore.recommendations.upperBody = [];
     weatherStore.recommendations.lowerBody = [];
@@ -103,53 +104,45 @@ function generateRecommendations()
 
     // Getting clothing recommendations
 
-    if(weatherStore.weatherInfo.temperature <= 32)
-    {
+    if (weatherStore.weatherInfo.temperature <= 32) {
         // Extreme cold
         weatherStore.recommendations.upperBody.push("Heavy Coat", "Sweater");
         weatherStore.recommendations.accessories.push("Gloves", "Hat");
         weatherStore.recommendations.lowerBody.push("Thick Pants");
         weatherStore.recommendations.footwear.push("Boots");
     }
-    else if(weatherStore.weatherInfo.temperature <= 60)
-    {
+    else if (weatherStore.weatherInfo.temperature <= 60) {
         // Moderate Cold
         weatherStore.recommendations.upperBody.push("Jacket", "Sweater");
         weatherStore.recommendations.lowerBody.push("Jeans", "Sweatpants");
     }
-    else if(weatherStore.weatherInfo.temperature <= 75)
-    {
+    else if (weatherStore.weatherInfo.temperature <= 75) {
         // Moderate
         weatherStore.recommendations.upperBody.push("TShirt", "Light Sweater");
         weatherStore.recommendations.lowerBody.push("Jeans", "Sweatpants");
     }
-    else
-    {
+    else {
         // Moderate to High Heat
         weatherStore.recommendations.upperBody.push("TShirt");
         weatherStore.recommendations.lowerBody.push("Shorts");
     }
 
     // Rain based recommendations (CAN BE EXPANDED UPON IF NEEDED)
-    if(weatherStore.weatherInfo.rainChance >= 60)
-    {
+    if (weatherStore.weatherInfo.rainChance >= 60) {
         weatherStore.recommendations.accessories.push("Umbrella");
     }
 
     // Wind based recommendations
-    if(weatherStore.weatherInfo.windSpeed >= 20)
-    {
+    if (weatherStore.weatherInfo.windSpeed >= 20) {
         weatherStore.recommendations.accessories.push("Windbreaker");
     }
 
     // Short forecast based recommendations
-    if(weatherStore.weatherInfo.shortForecast.toLowerCase().includes("sunny"))
-    {
+    if (weatherStore.weatherInfo.shortForecast.toLowerCase().includes("sunny")) {
         weatherStore.recommendations.accessories.push("Sunglasses");
     }
-    else if(weatherStore.weatherInfo.shortForecast.toLowerCase().includes("rain") & 
-            !(weatherStore.recommendations.accessories.includes("Umbrella")))
-    {
+    else if (weatherStore.weatherInfo.shortForecast.toLowerCase().includes("rain") &
+        !(weatherStore.recommendations.accessories.includes("Umbrella"))) {
         weatherStore.recommendations.accessories.push("Umbrella");
     }
 
@@ -157,15 +150,12 @@ function generateRecommendations()
 }
 
 // Beginning run based on current location
-async function initialization()
-{
-    try 
-    {
+async function initialization() {
+    try {
         const userPosition = await getCurrentLocation();
         await getHourWeather(userPosition[0], userPosition[1]);
     }
-    catch(error)
-    {
+    catch (error) {
         console.error("Error getting location and weather data", error);
     }
 }
