14145febfd0f0fbfecdec2a139526be45811f45b
jacers <jacerausch@gmail.com>
2024-12-06 10:34:20 -0600

    formatting changes

--- Scripts/Dashboard.js
+++ Scripts/Dashboard.js
@@ -6,7 +6,6 @@ import { addAllRecs } from './recommendations.js';
 const slides = []; 
 
 document.addEventListener('DOMContentLoaded', async () => {
-
     // Getting location of the user
     const userPosition = await getCurrentLocation();
 
@@ -17,14 +16,13 @@ document.addEventListener('DOMContentLoaded', async () => {
     updateWeatherDisplay()
 
     addAllRecs();
-
-    //updateSlidesWithWeather(); // Jacob you won't need to pass an argument through this anymore
 });
 
 // Toggling between fahrenheit and celcius
 document.getElementById('toggles').addEventListener('change', () => {
     const toggleUnits = document.getElementById('toggles');
     const tempElement = document.getElementById('temp');
+
     if (toggleUnits.checked) { // true when the units are in Fahrenheit
         toggleUnits.checked = true; // toggle state of the checkbox
         tempElement.textContent = tempElement.getAttribute('data-tempCelsius') + '° C';
@@ -44,16 +42,16 @@ function updateWeatherDisplay() {
     conditionElement.textContent = weatherStore.weatherInfo.shortForecast;
     windElement.textContent = weatherStore.weatherInfo.windSpeed;
     chanceOfRainElement.textContent = weatherStore.weatherInfo.rainChance;
-    if(!weatherStore.weatherInfo.rainChance)
-    {
+    
+    if(!weatherStore.weatherInfo.rainChance) {
         chanceOfRainElement.textContent = 0;
     }
+
     tempElement.setAttribute('data-tempFahrenheit', weatherStore.weatherInfo.temperature);
     tempElement.setAttribute('data-tempCelsius', Math.round((weatherStore.weatherInfo.temperature - 32) * 5 / 9));
     tempElement.textContent = `${weatherStore.weatherInfo.temperature}° F`;
     
-    
-     // Choose icon based on weather conditions
+    // Choose icon based on weather conditions
     let iconPath = "../icons/default.png"; // Default icon path
     const forecast = weatherStore.weatherInfo.shortForecast.toLowerCase();
 
@@ -61,8 +59,8 @@ function updateWeatherDisplay() {
         console.log('Daytime logic running...');
         // Define a prioritized mapping of keywords to icon paths
         const weatherIcons = [
-            { keyword: "fog",  path: "../images/fog_or_mist.png" },
-            { keyword: "haze",  path: "../images/haze.png" },
+            { keyword: "fog", path: "../images/fog_or_mist.png" },
+            { keyword: "haze", path: "../images/haze.png" },
             { keyword: "sleet", path: "../images/sleet.png" },
             { keyword: "freezing rain", path: "../images/sleet.png" },
             { keyword: "rain", path: "../images/rain.png" },
@@ -72,15 +70,16 @@ function updateWeatherDisplay() {
             { keyword: "hail", path: "../images/hail.png" },
             { keyword: "blizzard", path: "../images/blizzard.png" },
             { keyword: "thunderstorms", path: "../images/thunderstorms.png" },
-            { keyword: "thunder",  path: "../images/thunderstorms.png" },
-            { keyword: "storms",  path: "../images/thunderstorms.png" },
-            { keyword: "partly cloudy",  path: "../images/partly_cloudy.png" },
+            { keyword: "thunder", path: "../images/thunderstorms.png" },
+            { keyword: "storms", path: "../images/thunderstorms.png" },
+            { keyword: "partly cloudy", path: "../images/partly_cloudy.png" },
             { keyword: "mostly cloudy", path: "../images/mostly_cloudy.png" },
             { keyword: "mostly sunny", path: "../images/mostly_sunny.png" },
             { keyword: "partly sunny", path: "../images/mostly_cloudy.png"},
             { keyword: "sunny", path: "../images/sunny.png" },
             { keyword: "cloudy", path: "../images/cloudy.png" }
         ];
+
         // Match forecast with prioritized keywords
         for (const { keyword, path } of weatherIcons) {
             if (forecast.includes(keyword)) {
@@ -91,8 +90,8 @@ function updateWeatherDisplay() {
     } else {
         console.log('isDaytime is either false or undefined.');
         const weatherIcons = [
-            { keyword: "fog",  path: "../images/fog_or_mist.png" },
-            { keyword: "haze",  path: "../images/haze.png" },
+            { keyword: "fog", path: "../images/fog_or_mist.png" },
+            { keyword: "haze", path: "../images/haze.png" },
             { keyword: "sleet", path: "../images/sleet.png" },
             { keyword: "freezing rain", path: "../images/sleet.png" },
             { keyword: "rain", path: "../images/rain.png" },
@@ -100,16 +99,17 @@ function updateWeatherDisplay() {
             { keyword: "hail", path: "../images/hail.png" },
             { keyword: "blizzard", path: "../images/blizzard.png" },
             { keyword: "thunderstorms", path: "../images/thunderstorms.png" },
-            { keyword: "thunder",  path: "../images/thunderstorms.png" },
-            { keyword: "storms",  path: "../images/thunderstorms.png" },
+            { keyword: "thunder", path: "../images/thunderstorms.png" },
+            { keyword: "storms", path: "../images/thunderstorms.png" },
             { keyword: "isolated clouds", path: "../images/isolated_clouds_night.png"},
             { keyword: "mostly clear", path: "../images/partly_cloudy_night.png" },
-            { keyword: "partly cloudy",  path: "../images/partly_cloudy_night.png" },
-            { keyword: "mostly cloudy",  path: "../images/mostly_cloudy_night.png"},
+            { keyword: "partly cloudy", path: "../images/partly_cloudy_night.png" },
+            { keyword: "mostly cloudy", path: "../images/mostly_cloudy_night.png"},
             { keyword: "partly clear", path: "../images/mostly_cloudy_night.png"},
             { keyword: "clear", path: "../images/clear.png" },
             { keyword: "cloudy", path: "../images/cloudy.png" }
         ];
+
         // Match forecast with prioritized keywords
         for (const { keyword, path } of weatherIcons) {
             if (forecast.includes(keyword)) {
@@ -126,8 +126,8 @@ function updateWeatherDisplay() {
 
     const numberOfAlerts = weatherStore.weatherInfo.alerts.length;
 
+    // Create and insert the title if it doesn't exist
     if (!alertTitle) {
-        // Create and insert the title if it doesn't exist
         alertTitle = document.createElement('h2');
         alertTitle.id = 'alert-title'; // Assign an ID to identify it later
         alertTitle.style.textAlign = 'center'; // Center the title
--- Scripts/WeatherLogic.js
+++ Scripts/WeatherLogic.js
@@ -1,9 +1,7 @@
 import { getCurrentLocation } from './Dashboard.js';
 
 // Exported object so that information can be accessed anywhere
-
 const weatherStore = {
-
     // Recommendations based off of the generateRecommendations() function
     // These are arrays of { imagePath, description }
     recommendations: {
@@ -29,7 +27,8 @@ const weatherStore = {
 };
 
 
-// This function populates the weatherInfo object based on latitude and longitude and also generates recommendations based off the data gotten
+// This function populates the weatherInfo object based on latitude and longitude
+// and also generates recommendations based off the data gotten
 export async function getHourWeather(latitude, longitude) {
     try {
         // Getting points from the latitude and longitude
@@ -58,16 +57,20 @@ export async function getHourWeather(latitude, longitude) {
         const currentHour = forecastData.properties.periods[0];
 
         // Putting all data in it's proper place
-        weatherStore.weatherInfo.temperature = currentHour.temperature;
-        weatherStore.weatherInfo.tempUnit = currentHour.temperatureUnit;
-        weatherStore.weatherInfo.rainChance = currentHour.probabilityOfPrecipitation.value;
-        weatherStore.weatherInfo.dewPoint = currentHour.dewpoint.value;
-        weatherStore.weatherInfo.relativeHumidity = currentHour.relativeHumidity.value;
-        weatherStore.weatherInfo.windSpeed = currentHour.windSpeed;
-        weatherStore.weatherInfo.windDirection = currentHour.windDirection;
-        weatherStore.weatherInfo.shortForecast = currentHour.shortForecast;
-        weatherStore.weatherInfo.isDaytime = currentHour.isDaytime;
-        
+        Object.assign(weatherStore.weatherInfo, {
+            temperature: currentHour.temperature,
+            tempUnit: currentHour.temperatureUnit,
+            rainChance: currentHour.probabilityOfPrecipitation.value,
+            dewPoint: currentHour.dewpoint.value,
+            relativeHumidity: currentHour.relativeHumidity.value,
+            windSpeed: currentHour.windSpeed,
+            windDirection: currentHour.windDirection,
+            shortForecast: currentHour.shortForecast,
+            isDaytime: currentHour.isDaytime,
+            date: splitDate(currentHour.startTime)[0],
+            hour: splitDate(currentHour.startTime)[1],
+        });
+
         // Add any alerts to an array
         if (alertsData && alertsData.features) {
             weatherStore.weatherInfo.alerts = alertsData.features.map(alert => ({
@@ -79,81 +82,70 @@ export async function getHourWeather(latitude, longitude) {
             weatherStore.weatherInfo.alerts = [];
         }
 
-        const currentDateAndTime = splitDate(currentHour.startTime);
-        weatherStore.weatherInfo.date = currentDateAndTime[0];
-        weatherStore.weatherInfo.hour = currentDateAndTime[1];
-
-        //console.log(weatherStore.weatherInfo); // Temp for testing purposes
         generateRecommendations();
 
         // Clear search input
         document.getElementById('location-input').value = '';
-
     } catch (error) {
         console.error("Error fetching weather data:", error);
     }
 }
 
-// Splitting the date from the API into something that's actually useful and readable
+// Splitting the date from the API into a more readable format
 function splitDate(currentDate) {
     const [divorcedDate, timeWithOffset] = currentDate.split("T");
-
-    // Fixing time
     const time = timeWithOffset.split(":").slice(0, 1).join(":");
-
     return [divorcedDate, time];
 }
 
+// Generates clothing and accessory recommendations based on weatherInfo
 function generateRecommendations() {
-
     // Clearing previous recommendations so we don't get duplicates
     weatherStore.recommendations.upperBody = [];
     weatherStore.recommendations.accessories = [];
 
-    // Getting clothing recommendations
-    if (weatherStore.weatherInfo.temperature <= 32) {
+    const { temperature, rainChance } = weatherStore.weatherInfo;
+
+    // Getting clothing recommendations based on temperature
+    if (temperature <= 32) {
         // Extreme cold
         weatherStore.recommendations.upperBody.push(
-            {imagePath: "../images/very heavy.png", description: "Heavy Coat"},
-            {imagePath: "../images/heavy.png", description: "Jacket"}
-            );
+            { imagePath: "../images/very heavy.png", description: "Heavy Coat" },
+            { imagePath: "../images/heavy.png", description: "Jacket" }
+        );
         weatherStore.recommendations.accessories.push(
-            {imagePath: "../images/snowboots.png", description: "Boots"}
+            { imagePath: "../images/snowboots.png", description: "Boots" }
         );
-    }
-    else if (weatherStore.weatherInfo.temperature <= 60) {
-        // Moderate Cold
+    } else if (temperature <= 60) {
+        // Moderate cold
         weatherStore.recommendations.upperBody.push(
-            {imagePath: "../images/medium.png", description: "Warm Clothes"},
-            {imagePath: "../images/heavy.png", description: "Jacket"}
+            { imagePath: "../images/medium.png", description: "Warm Clothes" },
+            { imagePath: "../images/heavy.png", description: "Jacket" }
         );
-    }
-    else if (weatherStore.weatherInfo.temperature <= 75) {
+    } else if (temperature <= 75) {
         // Moderate
         weatherStore.recommendations.upperBody.push(
-            {imagePath: "../images/light.png", description: "T-Shirt"}
+            { imagePath: "../images/light.png", description: "T-Shirt" }
         );
         weatherStore.recommendations.accessories.push(
-            {imagePath: "../images/hydrate.png", description: "Water Bottle"}
+            { imagePath: "../images/hydrate.png", description: "Water Bottle" }
         );
-    }
-    else {
-        // Moderate to High Heat
+    } else {
+        // High heat
         weatherStore.recommendations.upperBody.push(
-            {imagePath: "../images/light.png", description: "T-Shirt"}
+            { imagePath: "../images/light.png", description: "T-Shirt" }
         );
         weatherStore.recommendations.accessories.push(
-            {imagePath: "../images/hydrate.png", description: "Water Bottle"}
+            { imagePath: "../images/hydrate.png", description: "Water Bottle" }
         );
     }
 
-    // Rain based recommendations (CAN BE EXPANDED UPON)
-    if (weatherStore.weatherInfo.rainChance >= 5) {
+    // Rain-based recommendations
+    if (rainChance >= 5) {
         weatherStore.recommendations.accessories.push(
-            {imagePath: "../images/umbrella.png", description: "Umbrella"}
+            { imagePath: "../images/umbrella.png", description: "Umbrella" }
         );
     }
-
 }
 
 export default weatherStore;
